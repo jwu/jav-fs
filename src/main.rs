a569850c1dd42e8e9e7a0290f66f7ccfa113e03f
Johnny Wu <wujie@xmfunny.com>
2022-07-07 10:54:49 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use glob::glob;
+
+fn main() {
+    for entry in glob("**/*").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+    return;
+}
