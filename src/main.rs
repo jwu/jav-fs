fdf69597e6d4026bbd13f895fe4d063bc4ad4bd2
Johnny Wu <wujie@xmfunny.com>
2022-07-10 23:55:32 +0800

    bug fixes

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,18 @@ use std::io::{stdout, Write};
 use crossterm::{QueueableCommand, ExecutableCommand, cursor, terminal};
 
 fn main() {
-    let mut stdout = stdout();
-    let mut count = 0;
-    let mut files = HashMap::new();
-    let re_video = Regex::new(r".*\.(mp4|mkv|wmv)").unwrap();
+    let re_video = Regex::new(r".*\.(?i)(mp4|mkv|wmv)").unwrap();
     let re_file = Regex::new(
         r"[[:alpha:]]+-\d+|[[:alpha:]]+\d+"
     ).unwrap();
 
+    let mut stdout = stdout();
+    let mut cnt = 0;
+    let mut video_cnt = 0;
+
+    let mut files = HashMap::new();
     let mut files_failed = HashMap::new();
+    let mut conflicts = Vec::new();
 
     stdout.execute(cursor::Hide).unwrap();
 
@@ -22,7 +25,7 @@ fn main() {
     // for entry in glob("//192.168.3.10/jav/media/hd/#三宮つばき/**/*").unwrap() {
         match entry {
             Ok(path) => {
-                count += 1;
+                cnt += 1;
 
                 if !path.is_file() {
                     continue;
@@ -33,15 +36,22 @@ fn main() {
                 if !re_video.is_match(filename) {
                     continue;
                 }
+                video_cnt += 1;
 
                 let mat = re_file.find(filename);
                 if !mat.is_none() {
                     let match_result = mat.unwrap().as_str().to_owned();
                     let fullpath = path.to_str().unwrap().to_owned();
-                    files.insert(match_result, fullpath);
+
+                    if !files.contains_key(&match_result) {
+                        files.insert(match_result, fullpath);
+                    } else {
+                        conflicts.push(fullpath);
+                    }
                 } else {
                     let match_result = mat.unwrap().as_str().to_owned();
                     let fullpath = path.to_str().unwrap().to_owned();
+
                     files_failed.insert(match_result, fullpath);
                 }
 
@@ -61,15 +71,22 @@ fn main() {
 
     stdout.execute(cursor::Show).unwrap();
 
-    println!("scanned files {:?}", count);
-    println!("processed files {:?}", files.len());
+    println!("scanned files {:?}", cnt);
+    println!("videos files {:?}", video_cnt);
+    println!("actual videos {:?}", files.len());
+    println!("failed videos {:?}", files_failed.len());
+
+    for entry in conflicts {
+        println!("{}", entry);
+    }
 
     // for (key, value) in files {
     //     println!("{}: {}", key, value);
     // }
-    for (key, value) in files_failed {
-        println!("{}: {}", key, value);
-    }
+
+    // for (key, value) in files_failed {
+    //     println!("{}: {}", key, value);
+    // }
 
     return;
 }
