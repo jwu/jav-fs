bf8b15ae1983b82197e65920f264d0f80d6c982a
Johnny Wu <wujie@xmfunny.com>
2022-07-10 13:22:06 +0800

    fix glob failed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // use std::{thread, time};
 use std::collections::{HashMap};
-use regex::Regex;
+use regex::{Regex};
 use glob::glob;
 use std::io::{stdout, Write};
 use crossterm::{QueueableCommand, ExecutableCommand, cursor, terminal};
@@ -10,11 +10,16 @@ fn main() {
     let mut count = 0;
     let mut files = HashMap::new();
     let re_video = Regex::new(r".*\.(mp4|mkv|wmv)").unwrap();
-    let re_file = Regex::new(r"[[:alpha:]]-\d").unwrap();
+    let re_file = Regex::new(
+        r"[[:alpha:]]+-\d+|[[:alpha:]]+\d+"
+    ).unwrap();
+
+    let mut files_failed = HashMap::new();
 
     stdout.execute(cursor::Hide).unwrap();
 
     for entry in glob("//192.168.3.10/jav/media/**/*").unwrap() {
+    // for entry in glob("//192.168.3.10/jav/media/hd/#三宮つばき/**/*").unwrap() {
         match entry {
             Ok(path) => {
                 count += 1;
@@ -31,12 +36,14 @@ fn main() {
 
                 let mat = re_file.find(filename);
                 if !mat.is_none() {
-                    let str = mat.unwrap().as_str();
-                    files.insert(str, str);
+                    let match_result = mat.unwrap().as_str().to_owned();
+                    let fullpath = path.to_str().unwrap().to_owned();
+                    files.insert(match_result, fullpath);
+                } else {
+                    let match_result = mat.unwrap().as_str().to_owned();
+                    let fullpath = path.to_str().unwrap().to_owned();
+                    files_failed.insert(match_result, fullpath);
                 }
-                // if re_file.is_match(filename) {
-                //     files.insert(path.clone(), path.clone());
-                // }
 
                 // show process
                 stdout.queue(cursor::SavePosition).unwrap();
@@ -56,6 +63,13 @@ fn main() {
 
     println!("scanned files {:?}", count);
     println!("processed files {:?}", files.len());
+
+    // for (key, value) in files {
+    //     println!("{}: {}", key, value);
+    // }
+    for (key, value) in files_failed {
+        println!("{}: {}", key, value);
+    }
 
     return;
 }
